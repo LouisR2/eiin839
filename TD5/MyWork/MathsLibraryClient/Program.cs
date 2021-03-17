1d8ea6db0e6d70905fdeff2b46f0df25139953d8
Louis R <louis.raffin@etu.univ-cotedazur.fr>
2021-03-17 16:19:47 +0100

    add TD5 Intro & SOAP WebService

---
+++ TD5/MyWork/MathsLibraryClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ServiceReference1;
+
+namespace MathsLibraryClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Test client");
+
+            MathsOperationsClient client = new MathsOperationsClient();
+            int res = await client.AddAsync(1, 2);
+
+            Console.WriteLine(res);
+        }
+    }
+}
